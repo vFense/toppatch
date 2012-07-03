1a2999fce1c5fb6b83006f2e5002453bfb0633a3
Allen Sanabria <allen@toppatch.com>
2012-07-02 21:53:31 -0400

    TopPatch Initialized Environment

---
+++ mysql/percona-5.5/mysql-test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /opt/TopPatch/Percona-Server-5.5.23-rel25.3/mysql-test
+# Build directory: /opt/TopPatch/Percona-Server-5.5.23-rel25.3/mysql-test
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
