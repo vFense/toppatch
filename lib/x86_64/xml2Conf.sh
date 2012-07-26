320d55fcb05f719e927a36057863fc1e5820edf0
Allen Sanabria <allen@fedora.toppatch>
2012-07-26 18:08:10 -0400

    Batteries are now included

---
+++ lib/x86_64/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/opt/TopPatch/lib/x86_64"
+XML2_LIBS="-lxml2    -lm "
+XML2_INCLUDEDIR="-I/opt/TopPatch/include/libxml2"
+MODULE_VERSION="xml2-2.8.0"
+
