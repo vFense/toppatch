00cca740e73b9f0c051b9046bc2ba89d326d2396
Luis Aleman <Luis@toppatch.com>
2012-08-16 08:55:16 -0400

    Added jQuery UI, CSS Tweaks
    Changed header style to contain the company logo.
    Renamed the brand image files.
    Added an expérimental sidebar page in test-html

---
+++ tp/wwwstatic/js/libs/jquery-ui.custom.js
@@ -0,0 +1,5 @@
+//@codekit-append "jquery-ui/jquery.ui.core.js";
+//@codekit-append "jquery-ui/jquery.ui.widget.js";
+//@codekit-append "jquery-ui/jquery.ui.mouse.js";
+//@codekit-append "jquery-ui/jquery.ui.position.js";
+//@codekit-append "jquery-ui/jquery.ui.sortable.js"; 
