4692a52ab56ac29e816eeecf8afe29ed57e07e1a
Luis Aleman <Luis@toppatch.com>
2012-08-24 16:11:13 -0400

    Moved jQuery UI compile location to within the jQuery UI directory

--- tp/wwwstatic/js/libs/jquery-ui.custom.js
+++
@@ -1,5 +0,0 @@
-//@codekit-append "jquery-ui/jquery.ui.core.js";
-//@codekit-append "jquery-ui/jquery.ui.widget.js";
-//@codekit-append "jquery-ui/jquery.ui.mouse.js";
-//@codekit-append "jquery-ui/jquery.ui.position.js";
-//@codekit-append "jquery-ui/jquery.ui.sortable.js"; 
