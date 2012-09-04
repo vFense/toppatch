de7ccbc653a7affe3d67fb8c3ea542e90abbfe7f
Luis Aleman <Luis@toppatch.com>
2012-09-04 10:56:57 -0400

    Renamed app script to application.js

---
+++ tp/wwwstatic/js/application.min.js
@@ -0,0 +1,14 @@
+// Top Patch Object
+var $application = {
+	// Semantic versioning
+	// See: http://semver.org/
+	"version": "0.1.0",
+	"versionDescription": "Initial Development"
+	"models": {},
+	"views": {},
+	"controllers": {}
+};
+
+//@codekit-append "application/timeSeriesChart.js";
+//@codekit-append "application/barCharts.js";
+//@codekit-append "application/tableGenerator.js";
