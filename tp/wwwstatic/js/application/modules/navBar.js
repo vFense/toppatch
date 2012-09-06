ef7930b6b0102bdbd688301cf6e41fa202c0c9a1
Luis Aleman <Luis@toppatch.com>
2012-09-06 13:56:13 -0400

    Module to control the navBar
    Should be made more generic...

---
+++ tp/wwwstatic/js/application/modules/navBar.js
@@ -0,0 +1,52 @@
+define(
+	['jquery', 'backbone', './navButton'],
+	function ($, Backbone, navButton) {
+		"use strict";
+		var NavBar = {
+			ButtonSet: Backbone.Collection.extend({
+				model: navButton.Model
+			}),
+			View: Backbone.View.extend({
+				el: $('<ul>')
+					.addClass('nav')
+					.appendTo('#dashboardNav'),
+				events: {},
+				initialize: function () {
+					this.collection = new NavBar.ButtonSet(window.TopPatch.user.access);
+					this.render();
+				},
+				render: function () {
+					var that = this;
+					this.$el.html('');
+					_.each(this.collection.models, function (item) {
+						that.renderButton(item);
+					}, this);
+				},
+				renderButton: function (item) {
+					var buttonView = new navButton.View({
+						model: item
+					});
+					this.$el.append(buttonView.render().el);
+				},
+				setActive: function (e) {
+					// Check to make sure e and e.target are defined.
+					if(e && e.target) {
+						// The target should always be an anchor element
+						// We want its parent li
+						var $clicked = $(e.target).parent();
+						
+						// Check to see if the li element is part of this view's created element
+						if ( $($clicked, this.$el).length === 1 ) {
+							// If the li is not the active li, continue
+							if(!$clicked.hasClass('active')) {
+								// Set the li to active, and its siblings to not active
+								$clicked.toggleClass('active', true).siblings().toggleClass('active',false);
+							}
+						}
+					}
+				}
+			})
+		};
+		return NavBar;
+	}
+);
