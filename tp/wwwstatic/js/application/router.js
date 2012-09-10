61047996d36ec28499f91b3b767a2bea513182c6
Luis Aleman <Luis@toppatch.com>
2012-09-10 11:42:12 -0400

    Remove unneeded comments

--- tp/wwwstatic/js/application/router.js
+++ tp/wwwstatic/js/application/router.js
@@ -22,12 +22,10 @@ define(
 				this.viewManager = new app.ViewManager('#dashboard-view');
 			},
 			home: function () {
-				// Set the active Dashboard Nav Button
 				// Update the main dashboard view
 				this.viewManager.showView({ el: 'Home', render: function () { return true; }, close: function () { return true; }});
 			},
 			showPatchAdmin: function () {
-				// Set the active Dashboard Nav Button
 				// Update the main dashboard view
 				this.viewManager.showView({ el: 'Patch Admin', render: function () { return true; }, close: function () { return true; }});
 			},
