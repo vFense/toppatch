fae66983f11fad7af1e52109ba66152c4d4dcd46
Luis Aleman <Luis@toppatch.com>
2012-09-10 11:40:21 -0400

    Direct control of the nav bar removed

--- tp/wwwstatic/js/application/router.js
+++ tp/wwwstatic/js/application/router.js
@@ -16,9 +16,6 @@ define(
 				'*other':     'defaultAction'
 			},
 			initialize: function () {
-				// Start the Dashboard Nav Bar
-				this.navBar = new NavBar.View();
-				
 				// Create a new ViewManager with #dashboard-view as its target element
 				// All views sent to the ViewManager will render in the target element
                 this.vent = app.vent;
@@ -26,17 +23,11 @@ define(
 			},
 			home: function () {
 				// Set the active Dashboard Nav Button
-				this.navBar.setActive({target:$('#dashboardNav a[href$="dashboard"]')});
-				
 				// Update the main dashboard view
-				this.viewManager.showView({ el: 'Home', render: function () {return true; }, close: function () { return true; }});
 			},
 			showPatchAdmin: function () {
 				// Set the active Dashboard Nav Button
-				this.navBar.setActive({target:$('#dashboardNav a[href$="patchAdmin"]')});
-				
 				// Update the main dashboard view
-				this.viewManager.showView({ el: 'Patch Admin', render: function () {return true; }, close: function () { return true; }});
 			},
 			defaultAction: function (other) {
 				this.viewManager.showView({ el: '404: Not Found', render: function () { return true; }, close: function () {  return true; }});
