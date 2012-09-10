03daccb392dfa99e2c4f4467e832043568c55f99
Luis Aleman <Luis@toppatch.com>
2012-09-10 11:23:59 -0400

    Page header and footer templates and javascript

---
+++ tp/wwwstatic/js/application/modules/pageFooter.js
@@ -0,0 +1,21 @@
+define(
+	['jquery', 'backbone', 'text!templates/pageFooter.html'],
+	function ($, Backbone, footerTemplate) {
+		"use strict";
+		var PageFooter = {};
+		PageFooter.View = Backbone.View.extend({
+			el: $('<footer>')
+				.attr('id', 'pageFooter'),
+			template: footerTemplate,
+			initialize: function () {
+				this.model = window.User;
+			},
+			render: function () {
+				var tmpl = _.template(this.template);
+				this.$el.prepend(tmpl(this.model.toJSON()));
+				return this;
+			}
+		});
+		return PageFooter;
+	}
+);
---
+++ tp/wwwstatic/js/application/modules/pageHeader.js
@@ -0,0 +1,24 @@
+define(
+	['jquery', 'backbone', 'modules/navBar', 'text!templates/pageHeader.html'],
+	function ($, Backbone, DashNav, headerTemplate) {
+		"use strict";
+		var PageHeader = {};
+		PageHeader.View = Backbone.View.extend({
+			el: $('<header>')
+				.attr('id', 'pageHeader'),
+			template: headerTemplate,
+			initialize: function () {
+				this.model = window.User;
+			},
+			events: {
+				'change': 'render'
+			},
+			render: function () {
+				var tmpl = _.template(this.template);
+				this.$el.html(tmpl(this.model.toJSON()));
+				return this;
+			}
+		});
+		return PageHeader;
+	}
+);
