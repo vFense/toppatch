d1d69d3e42569b62d51f54c2b05d664da1976a76
Luis Aleman <Luis@toppatch.com>
2012-09-11 20:45:16 -0400

    Conditional formatting is working
    May implement logic for multiple conditions in the future

--- tp/wwwstatic/js/application/modules/overviewDetail.js
+++ tp/wwwstatic/js/application/modules/overviewDetail.js
@@ -18,14 +18,59 @@ define(
                 },
                 initialize: function () {},
                 render: function () {
-                    var tmpl = _.template(this.template);
+                    var tmpl = _.template(this.template),
+                        format = this.model.get('format'),
+                        that = this;
                     this.$el
                         .html(tmpl(this.model.toJSON()));
-                        //.toggleClass(
-                        //    this.model.get('accent'),
-                        //    this.model.get('accentFn')(this.model.get('data'))
-                        //)
+
+                    if (_.isObject(format) && !_.isEmpty(format)) {
+                        this.applyFormat();
+                    }
+
                     return this;
+                },
+                applyFormat: function () {
+                    var data = this.model.get('data'),
+                        format = this.model.get('format')[0],
+                        rule  = format.rule,
+                        value = format.value,
+                        style = format.style,
+                        apply = false;
+
+                    if (rule === 'e') {
+                        // If data === value
+                        if (data === value) {
+                            apply = true;
+                        }
+                    } else if (rule === 'ne') {
+                        // If data !== value
+                        if (data !== value) {
+                            apply = true;
+                        }
+                    } else if (rule === 'gt') {
+                        // If data > value
+                        if (data > value) {
+                            apply = true;
+                        }
+                    } else if (rule === 'lt') {
+                        // If data < value
+                        if (data < value) {
+                            apply = true;
+                        }
+                    } else if (rule === 'gte') {
+                        // If data >= value
+                        if (data >= value) {
+                            apply = true;
+                        }
+                    } else if (rule === 'lte') {
+                        // If data <= value
+                        if (data <= value) {
+                            apply = true;
+                        }
+                    }
+
+                    this.$el.toggleClass(style, apply);
                 }
             })
         };
