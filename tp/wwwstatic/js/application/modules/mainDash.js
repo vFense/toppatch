622b7dbe1d5162e4addab861df68d4b522035f0f
Luis Aleman <Luis@toppatch.com>
2012-09-12 02:14:05 -0400

    Changed "mainDash" variable to "exports"
    Conforms with other files
    Also normalized tabs to 4 spaces

--- tp/wwwstatic/js/application/modules/mainDash.js
+++ tp/wwwstatic/js/application/modules/mainDash.js
@@ -2,29 +2,29 @@ define(
     ['jquery', 'backbone', 'text!templates/mainDash.html', 'modules/overview' ],
     function ($, Backbone, myTemplate, Overview) {
         "use strict";
-        var mainDash = {};
-        mainDash.Model = Backbone.Model.extend({});
-        mainDash.View = Backbone.View.extend({
-            initialize: function () {
-                //this.model = window.tempModel = new mainDash.Model();
-                this.template = myTemplate;
-            },
-            render: function () {
-                var tmpl = _.template(this.template),
-                    that = this;
+        var exports = {
+            Model: Backbone.Model.extend({}),
+            View: Backbone.View.extend({
+                initialize: function () {
+                    this.template = myTemplate;
+                },
+                render: function () {
+                    var tmpl = _.template(this.template),
+                        that = this;
 
-                this.$el.html('');
+                    this.$el.html('');
 
-                this.overview = new Overview.View({
-                    el: $('<summary>').addClass('row-fluid clearfix')
-                });
-                this.$el.append(that.overview.render().$el);
+                    this.overview = new Overview.View({
+                        el: $('<summary>').addClass('row-fluid clearfix')
+                    });
+                    this.$el.append(that.overview.render().$el);
 
-                this.$el.append(tmpl());
+                    this.$el.append(tmpl());
 
-                return this;
-            }
-        });
-        return mainDash;
+                    return this;
+                }
+            })
+        };
+        return exports;
     }
 );
