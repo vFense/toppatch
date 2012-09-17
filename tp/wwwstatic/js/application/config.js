ae92200107293e7b91596eb64033a92506668132
Luis Aleman <Luis@toppatch.com>
2012-09-17 12:42:01 -0400

    New jQuery UI super shim
    Created a set of shims that can load individual jQuery UI modules, and
    their dependencies.
    
    Datepicker requires at least one localization to work. Currently there
    are no localizations included so DatePicker will not work.

--- tp/wwwstatic/js/application/config.js
+++ tp/wwwstatic/js/application/config.js
@@ -6,12 +6,31 @@ require.config({
         // Create shortcuts
         // i.e. Use 'jquery' insteaad of typing out 'libs/jquery/jquery-1.8.1.min'
         'jquery'            : '../libs/jquery/jquery-1.8.1',
-        'jquery.ui'         : '../libs/jquery-ui/jquery-ui.custom.min',
+        //'jquery.ui'         : '../libs/jquery-ui/jquery-ui.custom.min',
         'jquery.bootstrap'  : '../libs/bootstrap/bootstrap.min',
         'underscore'        : '../libs/underscore/underscore',
         'backbone'          : '../libs/backbone/backbone',
         'd3'                : '../libs/d3/d3.v2',
-        'text'              : '../libs/require/plugins/text'
+        'text'              : '../libs/require/plugins/text',
+        
+        // jQuery.ui modules
+        'jquery.ui.Core'        : '../libs/jquery-ui/dev/jquery.ui.Core',
+		'jquery.ui.Widget'      : '../libs/jquery-ui/dev/jquery.ui.Widget',
+		'jquery.ui.Mouse'       : '../libs/jquery-ui/dev/jquery.ui.Mouse',
+		'jquery.ui.Position'    : '../libs/jquery-ui/dev/jquery.ui.Position',
+		'jquery.ui.Draggable'   : '../libs/jquery-ui/dev/jquery.ui.Draggable',
+		'jquery.ui.Droppable'   : '../libs/jquery-ui/dev/jquery.ui.Droppable',
+		'jquery.ui.Resizable'   : '../libs/jquery-ui/dev/jquery.ui.Resizable',
+		'jquery.ui.Selectable'  : '../libs/jquery-ui/dev/jquery.ui.Selectable',
+		'jquery.ui.Sortable'    : '../libs/jquery-ui/dev/jquery.ui.Sortable',
+		'jquery.ui.Accordion'   : '../libs/jquery-ui/dev/jquery.ui.Accordion',
+		'jquery.ui.Autocomplete': '../libs/jquery-ui/dev/jquery.ui.Autocomplete',
+		'jquery.ui.Button'      : '../libs/jquery-ui/dev/jquery.ui.Button',
+		'jquery.ui.Dialog'      : '../libs/jquery-ui/dev/jquery.ui.Dialog',
+		'jquery.ui.Slider'      : '../libs/jquery-ui/dev/jquery.ui.Slider',
+		'jquery.ui.Tabs'        : '../libs/jquery-ui/dev/jquery.ui.Tabs',
+		'jquery.ui.Datepicker'  : '../libs/jquery-ui/dev/jquery.ui.Datepicker',
+		'jquery.ui.Progressbar' : '../libs/jquery-ui/dev/jquery.ui.Progressbar'
     },
 
     shim: {
@@ -29,6 +48,25 @@ require.config({
         },
         'd3': {
             exports: 'd3'
-        }
+        },
+        
+        // jQuery.ui modules
+        'jquery.ui.Core'        : {exports: 'jQuery'},
+        'jquery.ui.Widget'      : {exports: 'jQuery'},
+        'jquery.ui.Mouse'       : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget']},
+        'jquery.ui.Position'    : {exports: 'jQuery'},
+        'jquery.ui.Draggable'   : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget', 'jquery.ui.Mouse']},
+        'jquery.ui.Droppable'   : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget', 'jquery.ui.Mouse', 'jquery.ui.Draggable']},
+        'jquery.ui.Resizable'   : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget', 'jquery.ui.Mouse']},
+        'jquery.ui.Selectable'  : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget', 'jquery.ui.Mouse']},
+        'jquery.ui.Sortable'    : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget', 'jquery.ui.Mouse']},
+        'jquery.ui.Accordion'   : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget']},
+        'jquery.ui.Autocomplete': {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget', 'jquery.ui.Position']},
+        'jquery.ui.Button'      : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget']},
+        'jquery.ui.Dialog'      : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget', 'jquery.ui.Position']},
+        'jquery.ui.Slider'      : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget', 'jquery.ui.Mouse']},
+        'jquery.ui.Tabs'        : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget']},
+        'jquery.ui.Datepicker'  : {exports: 'jQuery', deps: ['jquery.ui.Core']},
+        'jquery.ui.Progressbar' : {exports: 'jQuery', deps: ['jquery.ui.Core', 'jquery.ui.Widget']}
     }
 });
