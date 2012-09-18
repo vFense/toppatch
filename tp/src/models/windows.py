6c8d62b02a0bb44aed6c1916e16ebd21663eb864
Allen Sanabria <allen@toppatch.com>
2012-09-17 20:13:49 -0400

    Fixed Duplicate NodeInfo entry in windows.py

--- tp/src/models/windows.py
+++ tp/src/models/windows.py
@@ -5,31 +5,6 @@ from sqlalchemy.dialects.mysql import BIGINT, BOOLEAN, CHAR, DATETIME, TEXT, TIN
 from sqlalchemy.orm import relationship, backref
 
 
-class NodeInfo(Base):
-    """
-    Represents one row from the hosts table.
-    """
-    __tablename__ = "node_info"
-    __visit_name__ = "column"
-    __table_args__ = {
-        'mysql_engine': 'InnoDB',
-        'mysql_charset': 'utf8'
-    }
-    id = Column(BIGINT(unsigned=True),primary_key=True)
-    host_name = Column(VARCHAR(32), nullable=False)
-    ip_address = Column(VARCHAR(16), nullable=False)
-    operating_system = Column(VARCHAR(32), nullable=False)
-    os_version = Column(VARCHAR(32), nullable=False)
-    host_status = Column(BOOLEAN)   # True = On, False = Off
-    def __init__(self, ip_address, host_name, operating_system,os_version, host_status=False):
-        self.host_name = host_name
-        self.ip_address = ip_address
-        self.operating_system = operating_system
-        self.os_version = os_version
-        self.host_status = host_status
-    def __repr__(self):
-        return "<NodeInfo(%s, %s, %s, %s, %s)>" %(self.host_name, self.ip_address, self.operating_system,self.os_version, self.host_status)
-
 class WindowsUpdate(Base):
     """
     Represents an application that is installed on a node, as oppose to a Product from a Vendor in the database.
