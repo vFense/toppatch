6ebb01a889b755b51d1fd02a6d84b676af5d4bf7
Allen Sanabria <allen@toppatch.com>
2012-09-21 23:56:21 -0400

    Restructured the NodesInfo Table. Added the Results and Operations Table. Fixed some minor formatting

--- tp/src/models/node.py
+++ tp/src/models/node.py
@@ -18,18 +18,104 @@ class NodeInfo(Base):
     id = Column(BIGINT(unsigned=True),primary_key=True, autoincrement=True)
     host_name = Column(VARCHAR(32), nullable=False)
     ip_address = Column(VARCHAR(16), nullable=False)
-    operating_system = Column(VARCHAR(32), nullable=False)
-    os_version = Column(VARCHAR(32), nullable=False)
-    host_status = Column(BOOLEAN)   # True = On, False = Off
-    def __init__(self, ip_address, host_name, operating_system,os_version, host_status=False):
+    os_code = Column(VARCHAR(16), nullable=False)
+    os_string = Column(VARCHAR(32), nullable=False)
+    os_version_major = Column(VARCHAR(6), nullable=False)
+    os_version_minor = Column(VARCHAR(6), nullable=True)
+    os_version_build = Column(VARCHAR(8), nullable=True)
+    os_meta = Column(VARCHAR(32), nullable=True)
+    host_status = Column(BOOLEAN)   # True = Up, False = Down
+    agent_status = Column(BOOLEAN)   # True = Up, False = Down
+    last_agent_update = Column(DATETIME, nullable=False)
+    last_node_update = Column(DATETIME, nullable=False)
+    def __init__(self, ip_address, host_name, os_code,
+            os_string, os_version_major, os_version_minor,
+            os_version_build, os_meta, host_status=False,
+            agent_status=False, last_agent_update=None,
+            last_node_update=None
+            ):
         self.host_name = host_name
         self.ip_address = ip_address
-        self.operating_system = operating_system
-        self.os_version = os_version
+        self.os_code = os_code
+        self.os_string = os_string
+        self.os_version_major = os_version_major
+        self.os_version_minor = os_version_minor
+        self.os_version_build = os_version_build
+        self.os_meta = os_meta
         self.host_status = host_status
+        self.agent_status = agent_status
+        self.last_agent_update = last_agent_update
+        self.last_node_update = last_node_update
     def __repr__(self):
-        return "<NodeInfo(%s, %s, %s, %s, %s)>" %(self.host_name, self.ip_address, self.operating_system,self.os_version, self.host_status)
+        return "<NodeInfo(%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)>" %\
+                (
+                id, self.host_name, self.ip_address,
+                self.os_code, self.os_string, self.os_version_major,
+                self.os_version_minor, self.os_version_build,
+                self.os_meta, self.host_status, self.agent_status,
+                self.last_agent_update, self.last_node_update
+                )
 
+class Operations(Base):
+    """
+    Represents one row from the hosts table.
+    """
+    __tablename__ = "operations"
+    __visit_name__ = "column"
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8'
+    }
+    id = Column(BIGINT(unsigned=True),primary_key=True, autoincrement=True)
+    node_id = Column(BIGINT(unsigned=True),ForeignKey("node_info.id"))
+    results_id = Column(BIGINT(unsigned=True),
+        ForeignKey("results.id", use_alter=True,
+        name="fk_operations_result_id"))
+    operation_type = Column(VARCHAR(16), nullable=False)
+    operation_sent = Column(DATETIME, nullable=True)
+    operation_received = Column(DATETIME, nullable=True)
+    def __init__(self, operation_type,
+            operation_sent=None,
+            operation_received=None
+            ):
+        self.operation = operation
+        self.node_name = node_name
+        self.operation_sent = operation_sent
+        self.operation_received = operation_received
+    def __repr__(self):
+        return "<Operations(%d, %s, %s, %s, %s,)>" %\
+                (
+                id, node_id, results_id, self.operation_type, 
+                self.operation_sent,self.operation_received
+                )
+
+class Results(Base):
+    """
+    Represents one row from the hosts table.
+    """
+    __tablename__ = "results"
+    __visit_name__ = "column"
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8'
+    }
+    id = Column(BIGINT(unsigned=True),primary_key=True, autoincrement=True)
+    node_id = Column(BIGINT(unsigned=True),ForeignKey("node_info.id"))
+    operation_id = Column(BIGINT(unsigned=True),ForeignKey("operations.id"))
+    patch_id = Column(BIGINT(unsigned=True),
+        ForeignKey("windows_update.toppatch_id", use_alter=True,
+        name="fk_result_operations_id"))
+    result = Column(BOOLEAN)   # True = Pass, False = Failed
+    message = Column(VARCHAR(64), nullable=True)
+    def __init__(self, result, message):
+        self.result = result
+        self.message = message
+    def __repr__(self):
+        return "<Results(%d, %d, %d, %d, %s, %s)>" %\
+                (
+                id, node_id ,operation_id, patch_id,
+                self.result, self.message
+                )
 
 class SoftwareInstalled(Base):
     """
@@ -61,4 +147,10 @@ class SoftwareInstalled(Base):
         self.support_url = support_url
         self.date_installed = date_installed
     def __repr__(self):
-        return "<SoftwareInstalled(%s, %s, %s, %s, %s, %s, %s, %s)>" %(self.name, self.vendor, self.vendor_id, self.description, self.operating_system, self.version, self.support_url, self.date_installed)
+        return "<SoftwareInstalled(%d,%s,%s,%s,%s,%s,%s,%s,%s)>" %\
+                (
+                node_id, self.name, self.vendor, self.vendor_id,
+                self.description, self.operating_system,
+                self.version, self.support_url,
+                self.date_installed
+                )
