231033e9bec1d8eba9b2052bdbc412f2e5dc7878
Allen Sanabria <allen@toppatch.com>
2012-10-04 13:10:43 -0400

    VerifyJson now return False and None if the data is not valid

--- tp/src/receiver/tools/common.py
+++ tp/src/receiver/tools/common.py
@@ -2,8 +2,9 @@ from json import loads, dumps
 
 def verifyJsonIsValid(data):
     verified = True
+    json_data = None
     try:
         json_data = loads(data)
     except ValueError as e:
         verified = False
-    return(verified)
+    return(verified, json_data)
