f493e1e3f5946b2d6f8dbad06cf9443d5b0444ee
Allen Sanabria <allen@toppatch.com>
2012-10-09 22:18:59 -0400

    table changes

--- tp/src/models/windows.py
+++ tp/src/models/windows.py
@@ -16,9 +16,9 @@ class WindowsUpdate(Base):
         'mysql_charset': 'utf8'
     }
     toppatch_id = Column(INTEGER(unsigned=True),
-            primary_key=True, autoincrement=False,
-            unique=True
-            )
+        primary_key=True, autoincrement=False,
+        unique=True
+    )
     kb = Column(VARCHAR(32), nullable=False)
     vendor_id = Column(VARCHAR(128), nullable=False)
     title = Column(VARCHAR(128),nullable=False)
@@ -28,10 +28,10 @@ class WindowsUpdate(Base):
     date_pub = Column(DATETIME)
     file_size = Column(INTEGER(unsigned=True), nullable=False)
     def __init__(
-                self, toppatch_id, kb, vendor_id, title,
-                description,support_url, severity, date_pub,
-                file_size
-                ):
+            self, toppatch_id, kb, vendor_id, title,
+            description,support_url, severity, date_pub,
+            file_size
+    ):
         self.toppatch_id = toppatch_id
         self.kb = kb
         self.vendor_id = vendor_id
@@ -43,11 +43,11 @@ class WindowsUpdate(Base):
         self.file_size = file_size
     def __repr__(self):
         return "<WindowsUpdate(%s,%s,%s,%s,%s,%s,%s,%s,%s)>" %\
-                (
-                self.toppatch_id, self.kb, self.vendor_id, 
-                self.title, self.description, self.support_url,
-                self.severity, self.date_pub, self.file_size
-                )
+               (
+                   self.toppatch_id, self.kb, self.vendor_id,
+                   self.title, self.description, self.support_url,
+                   self.severity, self.date_pub, self.file_size
+                   )
 
 class ManagedWindowsUpdate(Base):
     """
@@ -60,20 +60,20 @@ class ManagedWindowsUpdate(Base):
         'mysql_charset': 'utf8'
     }
     id = Column(INTEGER(unsigned=True),
-            primary_key=True, autoincrement=True)
+        primary_key=True, autoincrement=True)
     node_id = Column(INTEGER(unsigned=True),
-            ForeignKey("node_info.id"))
+        ForeignKey("node_info.id"))
     toppatch_id = Column(INTEGER(unsigned=True),
-            ForeignKey("windows_update.toppatch_id"))
+        ForeignKey("windows_update.toppatch_id"))
     hidden = Column(BOOLEAN)
     installed = Column(BOOLEAN)
     attempts = Column(INTEGER)
     pending = Column(BOOLEAN)
     date_installed = Column(DATETIME)
     def __init__(self, node_id, toppatch_id, date_installed=None,
-                hidden=False, installed=False, attempts=0,
-                pending=False
-                ):
+                 hidden=False, installed=False, attempts=0,
+                 pending=False
+    ):
         self.node_id = node_id
         self.toppatch_id = toppatch_id
         self.hidden = hidden
@@ -83,9 +83,8 @@ class ManagedWindowsUpdate(Base):
         self.pending = pending
     def __repr__(self):
         return "<ManagedWindowsUpdate(%s,%s,%s,%s,%s,%s,%s)>" %\
-                (
-                self.node_id, self.toppatch_id, self.hidden,
-                self.installed, self.date_installed, self.attempts,
-                self.pending
-                )
-
+               (
+                   self.node_id, self.toppatch_id, self.hidden,
+                   self.installed, self.date_installed, self.attempts,
+                   self.pending
+                   )
