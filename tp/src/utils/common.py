955ea5689f7a8e10fe0c65eecd565fc4796fb763
Allen Sanabria <allen@toppatch.com>
2012-10-10 20:31:12 -0400

    added getExpire from cert.... It is very very dirty

--- tp/src/utils/common.py
+++ tp/src/utils/common.py
@@ -1,6 +1,6 @@
 import re
 from json import loads, dumps
-from datetime import date
+from datetime import datetime
 
 def verifyJsonIsValid(data):
     verified = True
@@ -27,3 +27,8 @@ def returnBool(fake_bool):
     elif fake_bool == "false":
         real_bool = False
     return real_bool
+
+def getExpirefromCert(cert):
+    asn1_time = re.search(r'([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})', cert).group(1,2,3,4,5,6)
+    t = map(lambda x: int(x), asn1_time)
+    return datetime(t[0], t[1], t[2], t[3], t[4], t[5])
