4341157e715b1e9573a16bbdd3e53a41a20e1308
Allen Sanabria <allen@toppatch.com>
2012-10-24 23:46:36 +0000

    ichanges applied

--- tp/src/utils/tcpasync.py
+++ tp/src/utils/tcpasync.py
@@ -13,7 +13,7 @@ class TcpConnect():
     Connect to the remote agent, using the openssl
     library backed by Gevent.
     """
-    def __init__(self, host, msg, port=9003, secure=True, timeout=15):
+    def __init__(self, host, msg, port=9003, secure=True, timeout=60):
         self.secure = secure
         self.host = host
         self.msg = msg
