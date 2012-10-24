31d00d4699f10c88dcd2424fa389d77481c1cebb
Allen Sanabria <allen@toppatch.com>
2012-10-23 21:33:22 -0400

    preparing for the scheduler

--- tp/src/toppatch.py
+++ tp/src/toppatch.py
@@ -26,6 +26,11 @@ from tornado.options import define, options
 from twisted.internet.protocol import Protocol, Factory
 from twisted.internet import reactor
 
+from apscheduler.scheduler import Scheduler
+
+sched = Scheduler()
+sched.start()
+
 define("port", default=8000, help="run on port", type=int)
 define("debug", default=True, help="enable debugging features", type=bool)
 
