36e3dfbd722cf471c5701a73e22b4cf81c8d0d77
Luis Aleman <Luis@toppatch.com>
2012-11-08 17:32:16 -0500

    Latest updates with basic hello world modal dialogue

---
+++ tp/wwwstatic/js/application/modules/modals/admin/main.js
@@ -0,0 +1,73 @@
+define(
+    ['jquery', 'underscore', 'backbone', 'modals/panel'],
+    function ($, _, Backbone, panel) {
+        "use strict";
+        return {
+            View: Backbone.View.extend({
+                initialize: function (options) {
+                    this.panel = new panel.View({
+                        okText: 'Done'
+                    });
+
+                    this.panel.bind('ok', this.hide, this);
+                    this.panel.bind('close', this.hide, this);
+
+                    this.options = _.extend({
+                        // Defaults here
+                    }, options);
+                },
+
+                beforeRender: $.noop,
+                onRender: $.noop,
+                render: function () {
+                    if (this.beforeRender !== $.noop) { this.beforeRender(); }
+
+                    // Render a blank panel
+                    this.panel.render();
+
+                    var that    = this,
+                        $body   = this.panel.$modalBody,
+                        $header = this.panel.$modalHeader,
+                        $footer = this.panel.$modalFooter;
+
+                    $body.html('hello world');
+
+                    if (this.onRender !== $.noop) { this.onRender(); }
+
+                    this.rendered = true;
+
+                    return this;
+                },
+
+                show: function () {
+                    // Render the content first
+                    if (!this.rendered) {
+                        this.render();
+                    }
+
+                    // Show the complete panel
+                    this.panel.show();
+
+                    return this;
+                },
+
+                hide: function () {
+                    var that = this;
+
+                    // Wait for the panel to hide
+                    // Then destroy the related objects
+                    this.panel.$el.bind('hidden', function () {
+                        that.panel.close();
+                        that.close();
+                    });
+
+                    // Hide the panel
+                    this.panel.hide();
+
+                    // do not return 'this'
+                    // object is being destroyed
+                }
+            })
+        };
+    }
+);
---
+++ tp/wwwstatic/js/application/modules/modals/panel.js
@@ -0,0 +1,101 @@
+define(
+    ['jquery', 'underscore', 'backbone', 'text!templates/modals/panel.html'],
+    function ($, _, Backbone, myTemplate) {
+        "use strict";
+        var exports = {
+            View: Backbone.View.extend({
+                className: 'modal',
+
+                events: {
+                    'click .close': function(event) {
+                        event.preventDefault();
+
+                        this.trigger('close');
+                    },
+                    'click .cancel': function(event) {
+                        event.preventDefault();
+
+                        this.trigger('cancel');
+                    },
+                    'click .ok': function(event) {
+                        event.preventDefault();
+
+                        this.trigger('ok');
+                    }
+                },
+
+                initialize: function (options) {
+                    this.template = myTemplate;
+                    this.options = _.extend({
+                        okText: 'OK',
+                        cancelText: 'Cancel',
+                        allowCancel: true,
+                        escape: true,
+                        animate: false
+                    }, options);
+                },
+
+                beforeRender: $.noop,
+                onRender: $.noop,
+                render: function () {
+                    if (this.beforeRender !== $.noop) { this.beforeRender(); }
+
+                    var tmpl = _.template(this.template),
+                        $el = this.$el,
+                        views = this.options.views,
+                        that = this;
+
+                    this.$el.empty();
+
+                    this.$el.append(tmpl(this.options));
+
+                    if (this.options.animate) {
+                        $el.addClass('fade');
+                    }
+
+                    _.extend(this, {
+                        '$modalHeader': $el.find('.modal-header'),
+                        '$modalBody'  : $el.find('.modal-body'),
+                        '$modalFooter': $el.find('.modal-footer')
+                    });
+
+                    if (this.onRender !== $.noop) { this.onRender(); }
+
+                    this.rendered = true;
+
+                    return this;
+                },
+
+                show: function () {
+                    if (!this.rendered) {
+                        this.render();
+                    }
+
+                    var that = this,
+                        $el = this.$el;
+
+                    $el.modal({
+                        keyboard: this.options.allowCancel,
+                        backdrop: this.options.allowCancel ? true : 'static'
+                    });
+
+                    this.on('cancel', function() {
+                        that.close();
+                    });
+
+                    return this;
+                },
+
+                hide: function () {
+                    this.$el.modal('hide');
+                    return this;
+                },
+
+                beforeClose: function () {
+                    this.hide();
+                }
+            })
+        };
+        return exports;
+    }
+);
