f8819fe28573ca13b3addbfb3fb47f594b5adc6b
Luis Aleman <Luis@toppatch.com>
2012-11-12 13:05:43 -0500

    Call close on hidden

--- tp/wwwstatic/js/application/modules/modals/panel.js
+++ tp/wwwstatic/js/application/modules/modals/panel.js
@@ -54,6 +54,7 @@ define(
                         },
                         hidden: function () {
                             that.trigger('hidden');
+                            that.close();
                         }
                     });
 
