83d0104196efb7a844fc2203fec465a6df895add
Luis Aleman <Luis@toppatch.com>
2012-11-12 16:53:44 -0500

    Redo admin/main.js for new modal/panel.js
    Include new admin/main template

--- tp/wwwstatic/js/application/modules/modals/admin/main.js
+++ tp/wwwstatic/js/application/modules/modals/admin/main.js
@@ -1,20 +1,12 @@
 define(
-    ['jquery', 'underscore', 'backbone', 'modals/panel'],
-    function ($, _, Backbone, panel) {
+    ['jquery', 'underscore', 'backbone', 'text!templates/modals/admin/main.html'],
+    function ($, _, Backbone, myTemplate) {
         "use strict";
         return {
             View: Backbone.View.extend({
-                initialize: function (options) {
-                    this.panel = new panel.View({
-                        okText: 'Done'
-                    });
-
-                    this.panel.bind('ok', this.hide, this);
-                    this.panel.bind('close', this.hide, this);
-
-                    this.options = _.extend({
-                        // Defaults here
-                    }, options);
+                className: 'admin-main-view',
+                initialize: function () {
+                    this.template = myTemplate;
                 },
 
                 beforeRender: $.noop,
@@ -22,51 +14,18 @@ define(
                 render: function () {
                     if (this.beforeRender !== $.noop) { this.beforeRender(); }
 
-                    // Render a blank panel
-                    this.panel.render();
+                    var that = this,
+                        template = _.template(this.template),
+                        $el = this.$el;
 
-                    var that    = this,
-                        $body   = this.panel.$body,
-                        $header = this.panel.$header,
-                        $footer = this.panel.$footer;
-
-
-                    $body.html('hello world');
+                    $el.empty();
+                    $el.html(template({}));
 
                     if (this.onRender !== $.noop) { this.onRender(); }
 
                     this.rendered = true;
 
                     return this;
-                },
-
-                show: function () {
-                    // Render the content first
-                    if (!this.rendered) {
-                        this.render();
-                    }
-
-                    // Show the complete panel
-                    this.panel.show();
-
-                    return this;
-                },
-
-                hide: function () {
-                    var that = this;
-
-                    // Wait for the panel to hide
-                    // Then destroy the related objects
-                    this.panel.$el.bind('hidden', function () {
-                        that.panel.close();
-                        that.close();
-                    });
-
-                    // Hide the panel
-                    this.panel.hide();
-
-                    // do not return 'this'
-                    // object is being destroyed
                 }
             })
         };
