fa9d817700da372012ac5d9bca9522c0d8c62d51
Luis Aleman <Luis@toppatch.com>
2012-11-14 15:36:56 -0500

    Comment on rogue code block that should not be in the router

--- tp/wwwstatic/js/application/router.js
+++ tp/wwwstatic/js/application/router.js
@@ -165,11 +165,14 @@ define(
 
                 app.vent.trigger('navigation:' + this.viewTarget, settings.hash);
                 app.vent.trigger('domchange:title', settings.title);
+
+                // The following code block should not be here!
                 var schedule = $(document).find('input[name="schedule"]:checked');
                 if (schedule.data('popover')) {
                     schedule.data('popover').options.content.find('input[name=datepicker]').datepicker('destroy');
                     schedule.popover('destroy');
                 }
+                // End rogue code block
 
                 if ($.type(settings.view) === 'string') {
                     require([settings.view], function (myView) {
