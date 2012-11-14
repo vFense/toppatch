d1c9968fb2e37f3af17e71c917e60f2c1a31ade3
Luis Aleman <Luis@toppatch.com>
2012-11-14 15:46:14 -0500

    Remove redundant var declaration

--- tp/wwwstatic/js/application/modules/widget.js
+++ tp/wwwstatic/js/application/modules/widget.js
@@ -2,81 +2,84 @@ define(
     ['jquery', 'underscore', 'backbone', 'app', 'text!templates/widget.html' ],
     function ($, _, Backbone, app, myTemplate) {
         "use strict";
-        var currentSpan = 0,
-            exports = {
-                Model: Backbone.Model.extend({
-                    defaults: {
-                        span: 6,
-                        showMenu: false,
-                        menuItems: [
-                            { name: 'Properties', href: '#properties'},
-                            'divider',
-                            { name: 'Close Widget', href: '#close'}
-                        ],
-                        content: {
-                            header: 'Header',
-                            body: 'Content',
-                            footer: 'Footer'
-                        }
-                    },
-                    validate: function (attrs) {
-                        if (attrs.span < 1 || attrs.span > 12) {
-                            return "Unexpected span size. Expected: 1 <= span <=  12.";
-                        }
+        var currentSpan = 0;
+        return {
+            Model: Backbone.Model.extend({
+                defaults: {
+                    span: 6,
+                    showMenu: false,
+                    menuItems: [
+                        { name: 'Properties', href: '#properties'},
+                        'divider',
+                        { name: 'Close Widget', href: '#close'}
+                    ],
+                    content: {
+                        header: 'Header',
+                        body: 'Content',
+                        footer: 'Footer'
                     }
-                }),
-                View: Backbone.View.extend({
-                    tagName: 'article',
-                    className: 'widget',
-                    initialize: function () {
-                        this.template = myTemplate;
-                        this.model.bind('change:span', this.renderSpan, this);
-                        this.model.bind('change:showMenu', this.render, this);
+                },
+                validate: function (attrs) {
+                    var out;
 
-                        // Listen for app wide events
-                        this.vent = app.vent;
+                    if (attrs.span < 1 || attrs.span > 12) {
+                        out = "Unexpected span size. Expected: 1 <= span <=  12.";
+                    }
 
-                        this.vent.bind('edit:widgets', this.setEditable, this);
-                    },
-                    beforeRender: $.noop,
-                    onRender: $.noop,
-                    render: function () {
-                        if (this.beforeRender !== $.noop) { this.beforeRender(); }
+                    return out;
+                }
+            }),
+            View: Backbone.View.extend({
+                tagName: 'article',
+                className: 'widget',
+                initialize: function () {
+                    this.template = myTemplate;
+                    this.model.bind('change:span', this.renderSpan, this);
+                    this.model.bind('change:showMenu', this.render, this);
+
+                    // Listen for app wide events
+                    this.vent = app.vent;
+
+                    this.vent.bind('edit:widgets', this.setEditable, this);
+                },
+                beforeRender: $.noop,
+                onRender: $.noop,
+                render: function () {
+                    if (this.beforeRender !== $.noop) { this.beforeRender(); }
 
                     var tmpl = _.template(this.template);
 
-                        window.view = this;
+                    window.view = this;
 
-                        this.$el.empty();
+                    this.$el.empty();
 
-                        this.$el.append(tmpl(this.model.toJSON()));
+                    this.$el.append(tmpl(this.model.toJSON()));
 
-                        this.renderSpan();
+                    this.renderSpan();
 
-                        if (this.onRender !== $.noop) { this.onRender(); }
-                        return this;
-                    },
-                    renderSpan: function () {
-                        var newSpan = this.model.get('span');
-                        this.$el.toggleClass(
-                            // Remove old span class
-                            'span' + currentSpan,
-                            false
-                        ).toggleClass(
-                            // Add new span class
-                            'span' + this.model.get('span'),
-                            true
-                        );
-                        currentSpan = newSpan;
-                    },
-                    setEditable: function (bool) {
-                        if (_.isBoolean(bool)) {
-                            this.model.set('showMenu', bool);
-                            this.$el.toggleClass('editable', bool);
-                        }
+                    if (this.onRender !== $.noop) { this.onRender(); }
+                    return this;
+                },
+                renderSpan: function () {
+                    var newSpan = this.model.get('span');
+                    this.$el.toggleClass(
+                        // Remove old span class
+                        'span' + currentSpan,
+                        false
+                    ).toggleClass(
+                        // Add new span class
+                        'span' + this.model.get('span'),
+                        true
+                    );
+                    currentSpan = newSpan;
+                },
+                setEditable: function (bool) {
+                    if (_.isBoolean(bool)) {
+                        this.model.set('showMenu', bool);
+                        this.$el.toggleClass('editable', bool);
                     }
-                })
-            };
-        return exports;
+                }
+            })
+        };
     }
 );
