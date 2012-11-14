6ebf4b6af7b4e87ea35a818ff39e2f6731861848
Allen Sanabria <allen@toppatch.com>
2012-11-14 16:00:23 -0500

    timeblocker functionality is working

--- tp/src/utils/scheduler/timeBlocker.py
+++ tp/src/utils/scheduler/timeBlocker.py
@@ -10,6 +10,7 @@ from jsonpickle import encode
 from utils.agentoperation import AgentOperation
 from utils.common import *
 from utils.db.query_table import nodeExists
+from utils.db.update_table import addTimeBlock, removeTimeBlock
 from models.scheduler import *
 
 def timeBlockLister(session):
@@ -18,21 +19,84 @@ def timeBlockLister(session):
     if blocks:
         for block in blocks:
             days_enabled, days_not_enabled = returnDays(block.days)
+            if block.start_date:
+                start_date = '%s/%s/%s' % \
+                        (
+                        str(block.start_date.month).rjust(2,'0'),
+                        str(block.start_date.day).rjust(2,'0'),
+                        str(block.start_date.year)
+                        )
+            else:
+                start_date = block.start_date
+            if block.end_date:
+                end_date = '%s/%s/%s' % \
+                        (
+                        str(block.end_date.month).rjust(2,'0'),
+                        str(block.end_date.day).rjust(2,'0'),
+                        str(block.end_date.year)
+                        )
+            else:
+                end_date = block.end_date
+            if block.start_time:
+                start_time = '%s:%s' % \
+                        (
+                        str(block.start_time.hour).rjust(2,'0'),
+                        str(block.start_time.minute).ljust(2,'0')
+                        )
+            else:
+                start_time = block.start_time
+            if block.end_time:
+                end_time = '%s:%s' % \
+                        (
+                        str(block.end_time.hour).rjust(2,'0'),
+                        str(block.end_time.minute).ljust(2,'0')
+                        )
+            else:
+                end_time = block.end_time
+
+
             msg = {
                    'name' : block.name,
                    'enabled' : block.enabled,
-                   'start_date' : block.start_date,
-                   'end_date' : block.end_date,
-                   'start_time' : block.start_time,
-                   'duration' : block.duration,
-                   'days_enabled' : block.days_enabled,
-                   'days_disabled' : block.days_not_enabled
+                   'start_date' : start_date,
+                   'end_date' : end_date,
+                   'start_time' : start_time,
+                   'end_time' : end_time,
+                   'days_enabled' : days_enabled,
+                   'days_disabled' : days_not_enabled
                    }
-        encoded_msg = encode(msg)
-        windows.append(encoded_message)
+            encoded_msg = encode(msg)
+            windows.append(encoded_msg)
     else:
         windows = '{"message" : "There arent any windows"}'
     return windows
+
+def timeBlockRemover(session,msg):
+    """The message needs to be in a valid json format.
+    either you must pass the time block id or 
+    the time block label, start_date, and start_time
+    {"id" : "1"} 
+    or 
+    {"label" : "test block",
+    "start_date" : "12/15/2012",
+    "start_time" : "09:00 AM"
+    }
+    """
+    valid, json_msg = verifyJsonIsValid(msg)
+    if valid:
+        if 'id' in json_msg:
+            removed = removeTimeBlock(session, id=json_msg['id'])
+        elif 'label' and 'start_date' and 'start_time' in json_msg:
+            label = json_msg['label']
+            start_date = dateTimeParser(json_msg['start_date'])
+            start_time = dateTimeParser(json_msg['start_time'])
+            removed = removeTimeBlock(session, label=label, 
+                    start_date=start_date, start_time=start_time
+                    )
+        if removed:
+            message = '{pass : %s, message %s}' \
+                    % (removed[0], removed[1])
+        return removed
 
 def timeBlockAdder(session, msg):
     """The message needs to be in a valid json format.
@@ -45,7 +109,7 @@ def timeBlockAdder(session, msg):
         "start_date" : "11/14/2012",
         "end_date" : "11/14/2013", (OPTIONAL)
         "start_time" : "09:00 AM",
-        "duration" : "8",
+        "end_time" : "05:00 PM",
         "days" : "0111110"
         }
     """
@@ -64,13 +128,16 @@ def timeBlockAdder(session, msg):
            json_msg['end_date'] = dateParser(json_msg['end_date']) 
         if 'start_date' in json_msg:
            json_msg['start_date'] = dateParser(json_msg['start_date']) 
-        block_added, message, block = addBlock(session, json_msg['label'],
+        if 'start_time' in json_msg:
+           json_msg['start_time'] = dateTimeParser(json_msg['start_time']) 
+        if 'end_time' in json_msg:
+           json_msg['end_time'] = dateTimeParser(json_msg['end_time']) 
+        block_added, message, block = addTimeBlock(session, json_msg['label'],
                 json_msg['enabled'], json_msg['start_date'],
                 json_msg['end_date'], json_msg['start_time'],
-                json_msg['duration'], json_msg['days']
+                json_msg['end_time'], json_msg['days']
                 )
 
-        return encode({"message" : message,
-                      "label" : json_msg['label']
-                      })
+        return '{message : %s,label : %s}'\
+                    % (message, json_msg["label"])
 
