9545fde27ecf420735325b20dc5b0bc4bafd7ae9
Luis Aleman <Luis@toppatch.com>
2012-11-15 13:15:25 -0500

    jsLint friendly regular expression

--- tp/wwwstatic/js/application/modules/modals/panel.js
+++ tp/wwwstatic/js/application/modules/modals/panel.js
@@ -152,7 +152,7 @@ define(
 
                         // Save last fragment and go back to it on 'close'
                         last = router.getLastFragment();
-                        if (last === '' || /^admin.*/.test(last)) {
+                        if (last === '' || /^admin$|\/\w/.test(last)) {
                             this._lastURL = "dashboard";
                         } else {
                             this._lastURL = last;
