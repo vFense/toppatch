a0a7d4495d492a0c86b426ee1975d800b596fb82
Allen Sanabria <allen@toppatch.com>
2012-11-15 21:56:16 -0500

    change title to name and add linux tables

--- tp/src/models/linux.py
+++ tp/src/models/linux.py
@@ -5,11 +5,11 @@ from sqlalchemy.dialects.mysql import INTEGER, BOOLEAN, CHAR, DATETIME, TEXT, TI
 from sqlalchemy.orm import relationship, backref
 
 
-class WindowsUpdate(Base):
+class LinuxUpdate(Base):
     """
     Represents an application that is installed on a node, as oppose to a Product from a Vendor in the database.
     """
-    __tablename__ = "windows_update"
+    __tablename__ = "linux_update"
     __visit_name__ = "column"
     __table_args__ = {
         'mysql_engine': 'InnoDB',
@@ -18,17 +18,17 @@ class WindowsUpdate(Base):
     toppatch_id = Column(VARCHAR(32),
         primary_key=True, unique=True, nullable=False
     )
-    kb = Column(VARCHAR(32), nullable=False)
+    kb = Column(VARCHAR(32), nullable=True)
     vendor_id = Column(VARCHAR(128), nullable=False)
     name = Column(VARCHAR(1024),nullable=False)
     description = Column(VARCHAR(4098), nullable=True)
     support_url = Column(VARCHAR(128), nullable=True)
-    severity = Column(VARCHAR(16), nullable=False)
+    severity = Column(VARCHAR(16), nullable=True)
     date_pub = Column(DATETIME)
     file_size = Column(INTEGER(unsigned=True), nullable=False)
     def __init__(
             self, toppatch_id, kb, vendor_id, name,
-            description,support_url, severity, date_pub,
+            description, support_url, severity, date_pub,
             file_size
     ):
         self.toppatch_id = toppatch_id
@@ -48,11 +48,11 @@ class WindowsUpdate(Base):
                    self.severity, self.date_pub, self.file_size
                    )
 
-class ManagedWindowsUpdate(Base):
+class ManagedLinuxUpdate(Base):
     """
     Represents an application that is installed on a node, as oppose to a Product from a Vendor in the database.
     """
-    __tablename__ = "managed_windows_update"
+    __tablename__ = "managed_linux_update"
     __visit_name__ = "column"
     __table_args__ = {
         'mysql_engine': 'InnoDB',
@@ -63,7 +63,7 @@ class ManagedWindowsUpdate(Base):
     node_id = Column(INTEGER(unsigned=True),
         ForeignKey("node_info.id"))
     toppatch_id = Column(VARCHAR(32),
-        ForeignKey("windows_update.toppatch_id"))
+        ForeignKey("linux_update.toppatch_id"))
     hidden = Column(BOOLEAN)
     installed = Column(BOOLEAN)
     attempts = Column(INTEGER)
