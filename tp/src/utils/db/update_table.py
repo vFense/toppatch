986b3cbfe7f653beceb7422a54b7417a4dd9c3de
Allen Sanabria <allen@toppatch.com>
2012-11-16 22:18:36 -0500

    added deleteing of tag functionality

--- tp/src/utils/db/update_table.py
+++ tp/src/utils/db/update_table.py
@@ -256,7 +256,7 @@ def removeAllNodesFromTag(session, tag_name):
     else:
         return(False, "Tag %s does not exists" % (tag_name), tag_name)
 
-def removeTagsFromNode(session, tag_name, nodes=[]):
+def removeNodesFromTag(session, tag_name, nodes=[]):
     nodes_completed = []
     nodes_failed = []
     for node in nodes:
