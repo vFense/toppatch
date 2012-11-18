cad63c0b6700904f279afe7cac842a15f363a1ec
Luis Aleman <Luis@toppatch.com>
2012-11-18 02:11:28 -0500

    Continued log progress

--- tp/wwwstatic/js/application/modules/logs.js
+++ tp/wwwstatic/js/application/modules/logs.js
@@ -19,7 +19,7 @@ define(
                 },
                 parse: function (response) {
                     this.recordCount = response.count;
-                    return response.data || response;
+                    return response.data[0] || response;
                 },
                 initialize: function (options) {
                     if (options.params) {
@@ -81,19 +81,21 @@ define(
                     var newElement = function (element) {
                             return $(document.createElement(element));
                         },
-                        $div        = newElement('div').addClass('item row-fluid'),
+                        result = item.get('result'),
+
+                        $item       = newElement('div').addClass('item'),
+                        $div        = newElement('div').addClass('row-fluid'),
                         $operation  = newElement('small').addClass('span2'),
                         $desc       = newElement('span').addClass('desc span3'),
                         $error      = newElement('small').addClass('span2').html('&nbsp;'),
                         $date       = newElement('span').addClass('float-right offset3 span2 alignRight');
 
-                    /*
-                     <a href="#" rel="tooltip" data-placement="right" data-original-title="Text Here">Error</a>
-                    */
-
-                    $operation.html(item.get('operation').toUpperCase());
+                    $operation.append(item.get('operation').toUpperCase());
                     $desc.html(item.get('node_id'));
-                    $date.html(
+                    $date.html(item.get('operation_sent'));
+
+                    /*
+                     $date.html(
                         $.datepicker.formatDate(
                             'mm-dd-yy',
                             $.datepicker.parseDate(
@@ -102,8 +104,9 @@ define(
                             )
                         )
                     );
+                    */
 
-                    if (item.get('result') === 'failed') {
+                    if (_.isBoolean(result) && !result) {
                         $div.addClass('fail');
                         $error.append(
                             newElement('a')
@@ -114,7 +117,9 @@ define(
                         );
                     }
 
-                    return $div.append($operation, $desc, $error, $date);
+                    return $item.append(
+                        $div.append($operation, $desc, $error, $date)
+                    );
                 },
                 updateList: function (event) {
                     var that = this,
