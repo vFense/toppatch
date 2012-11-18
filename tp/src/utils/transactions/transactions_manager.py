392d8debe662e961cc1f3a24d4f3a954110fb20a
Devalishis <dev@toppatch.com>
2012-11-18 01:37:03 +0000

    added fix if operation_receive is none

--- tp/src/utils/transactions/transactions_manager.py
+++ tp/src/utils/transactions/transactions_manager.py
@@ -9,11 +9,13 @@ def retrieveTransactions(session):
     transactions = getTransactions(session)
     transaction = []
     for trans in transactions:
+        if trans[1][0].operation_received:
+            operation_received = operation_received.strftime("%m/%d/%Y %H:%M")
         if len(trans[1]) == 1:
             transaction.append({
                          "operation" : trans[1][0].operation_type,
                          "operation_sent" : trans[1][0].operation_sent.strftime("%m/%d/%Y %H:%M"),
-                         "operations_received" : trans[1][0].operation_received.strftime("%m/%d/%Y %H:%M"),
+                         "operations_received" : operation_received,
                          "node_id" : trans[1][0].node_id,
                          "results_received" : None,
                          "patch_id" : None,
@@ -25,7 +27,7 @@ def retrieveTransactions(session):
             transaction.append({
                          "operation" : trans[1][0].operation_type,
                          "operation_sent" : trans[1][0].operation_sent.strftime("%m/%d/%Y %H:%M"),
-                         "operations_received" : trans[1][0].operation_received.strftime("%m/%d/%Y %H:%M"),
+                         "operations_received" : operation_received,
                          "node_id" : trans[1][0].node_id,
                          "results_received" : trans[1][0].results_received.strftime("%m/%d/%Y %H:%M"),
                          "patch_id" : trans[1][1].patch_id,
