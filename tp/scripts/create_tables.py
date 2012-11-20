3446f51c0232c86b718228667ba716634049710d
Devalishis <dev@toppatch.com>
2012-11-20 12:32:31 -0500

    go

--- tp/scripts/create_tables.py
+++ tp/scripts/create_tables.py
@@ -14,9 +14,5 @@ from sqlalchemy import create_engine
 
 db = create_engine('mysql://root:topmiamipatch@127.0.0.1/toppatch_server')
 db.echo = True
-<<<<<<< HEAD
-#db.drop(TimeBlocker)
-=======
->>>>>>> 0419f5486431c5b59f1d20179e9470659cebcadd
-#Base.metadata.drop_all(db)
+Base.metadata.drop_all(db)
 Base.metadata.create_all(db)
