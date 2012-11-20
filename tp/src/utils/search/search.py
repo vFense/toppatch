f4f32c7f721dfa263d22e1147e34891c600562af
Devalishis <dev@toppatch.com>
2012-11-20 14:28:02 -0500

    search fixed typos

--- tp/src/utils/search/search.py
+++ tp/src/utils/search/search.py
@@ -22,13 +22,13 @@ is_os = {
         }
 
 def basicPackageSearch(session, query, column, count=None, offset=None):
-    session = validateSession
+    session = validateSession(session)
     query = re.sub(r'^|$', '%', query)
     data = []
     final_out = {}
     total_count = 0
     if column in valid_pkg_columns:
-        found_packages = session.query(Package).filter(valid_pkg_columns[description].like(query)).order_by(Package.toppatch_id.desc()).limit(count).offset(offset)
+        found_packages = session.query(Package).filter(valid_pkg_columns['description'].like(query)).order_by(Package.toppatch_id.desc()).limit(count).offset(offset)
         total_count = session.query(Package).count()
         for pkg in found_packages:
             nodes_done = session.query(PackagePerNode).filter(PackagePerNode.toppatch_id == pkg.toppatch_id).filter(PackagePerNode.installed == True).all()
@@ -37,7 +37,7 @@ def basicPackageSearch(session, query, column, count=None, offset=None):
             nodes_failed = session.query(PackagePerNode).filter(PackagePerNode.toppatch_id == pkg.toppatch_id).filter(PackagePerNode.attempts >= 1).all()
             data.append({
                 "id" : pkg.toppatch_id,
-                "date" : pkg.date_published,
+                "date" : str(pkg.date_pub),
                 "description" : pkg.description,
                 "name" : pkg.name,
                 "severity" : pkg.severity,
@@ -46,10 +46,10 @@ def basicPackageSearch(session, query, column, count=None, offset=None):
                 "nodes/pend" : nodes_pending,
                 "nodes/done" : nodes_done,
                 "vendor" : {"name" : pkg.vendor_id, "patchID" : ""}
-                }
-        final_count['count'] = total_count
-        final_count['data'] = data
-        return final_count
+                })
+        final_out['count'] = total_count
+        final_out['data'] = data
+        return final_out
 
 
 """def advancePackageSearch(session, query, column, is_installed=None,
