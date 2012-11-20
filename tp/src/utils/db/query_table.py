aa96bbf954360b8d5b0aff9902d6e3c234c86f9d
Allen Sanabria <allen@toppatch.com>
2012-11-20 21:09:35 +0000

    if

--- tp/src/utils/db/query_table.py
+++ tp/src/utils/db/query_table.py
@@ -4,30 +4,9 @@
 from datetime import datetime
 from socket import getfqdn
 from models.base import Base
-from models.account import *
 from models.windows import *
 from models.node import *
-from models.tagging import *
-from models.scheduler import *
 from models.ssl import *
-from utils.common import *
-
-
-def userExists(session, user_id=None, user_name=None):
-    if user_id:
-        user = session.query(Users).filter_by(id=user_id)
-    elif user_name:
-        user = session.query(Users).filter_by(username=user_name)
-    user_exists = user.first()
-    return(user, user_exists)
-
-def tagExists(session, tag_id=None, tag_name=None):
-    if tag_id:
-        tag = session.query(TagInfo).filter_by(id=tag_id)
-    elif tag_name:
-        tag = session.query(TagInfo).filter_by(tag=tag_name)
-    tag_exists = tag.first()
-    return(tag, tag_exists)
 
 def nodeExists(session, node_ip=None, node_id=None):
     node = None
@@ -45,36 +24,6 @@ def operationExists(session, oper_id):
         session.query(Operations).filter_by(id=oper_id)
     exists = oper.first()
     return(exists, oper)
-
-def timeBlockExists(session, id=None, label=None, start_date=None, start_time=None):
-    if id:
-        tb_object = \
-            session.query(TimeBlocker).filter_by(id=id)
-        tb = tb_object.first()
-    return(tb_object, tb)
-
-def timeBlockExistsToday(session, start_date=None, start_time=None):
-    if start_date and start_time:
-        tb_object = \
-            session.query(TimeBlocker).filter(TimeBlocker.start_time <= start_time).filter(TimeBlocker.end_time >= start_time)
-        tbs = tb_object.all()
-        today_is_blocked = False
-        for tb in tbs:
-            if tb:
-                days_blocked, days_not_blocked = returnDays(tb.days)
-                for day in days_blocked:
-                    if week_day[day] == str(start_date.weekday()):
-                        today_is_blocked = True
-                        json_out = {
-                                   "pass" : False,
-                                    "message" : "Time Block %s exists for this time frame" % (tb.name)
-                                   }
-                        return(today_is_blocked, tb, json_out)
-        json_out = {
-                    "pass" : True,
-                    "message" : "Time Block does not exists for this time frame %s" % (start_date.strftime('%m/%d/%Y'))
-                   }
-        return(today_is_blocked, "No time blocks were found", json_out)
 
 def operationExistsUsingNodeId(session, node_id, oper_type):
     oper = \
