abd8414acdfe60123b4ed40e0e6a0d8695cbc86c
Allen Sanabria <allen@toppatch.com>
2012-11-19 19:07:31 -0500

    create all

--- tp/scripts/create_tables.py
+++ tp/scripts/create_tables.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 from models.base import Base
-#from models.account import *
-#from models.oauth.token import *
-#from models.application import *
-#from models.scanner import *
-#from models.cve import *
+from models.account import *
+from models.oauth.token import *
+from models.application import *
+from models.scanner import *
+from models.cve import *
 from models.windows import *
 from models.linux import *
 from models.node import *
-#from models.ssl import *
-#from models.scheduler import *
-#from models.tagging import *
-#from sqlalchemy import create_engine
+from models.ssl import *
+from models.scheduler import *
+from models.tagging import *
+from sqlalchemy import create_engine
 
 db = create_engine('mysql://root:topmiamipatch@127.0.0.1/toppatch_server')
 db.echo = True
