87c594066d5a004f130c227b9935017c13f3486e
Allen Sanabria <allen@toppatch.com>
2012-11-24 00:02:31 -0500

    Added create scritp back into my repo

---
+++ tp/scripts/create_tables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from models.base import Base
+from models.account import *
+from models.oauth.token import *
+from models.application import *
+from models.scanner import *
+from models.cve import *
+from models.packages import *
+from models.node import *
+from models.ssl import *
+from models.scheduler import *
+from models.tagging import *
+from sqlalchemy import create_engine
+
+db = create_engine('mysql://root:topmiamipatch@127.0.0.1/toppatch_server')
+db.echo = True
+#Base.metadata.drop_all(db)
+Base.metadata.create_all(db)
