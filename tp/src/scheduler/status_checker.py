b1e66534eda1a9e2b2fa34fa88b2ef9f375ba3ac
Devalishis <dev@toppatch.com>
2012-12-01 21:28:25 -0500

    99% pep8 compliant, All of my code has been thoroughly tested, frmo the csr operations to updates, to tagging, to scheduling, to timeblocking, to installing, rebooting, removing tags, removing schedules, removing timeblocks.\n I still need to make sure al of my print statements are pep8

--- tp/src/scheduler/status_checker.py
+++ tp/src/scheduler/status_checker.py
@@ -23,10 +23,10 @@ def agent_status():
             timediff = datetime.now() - node.last_agent_update
             if timediff.seconds > 480:
                 node.agent_status = False
-                print "AGENT IS DOWN, %d seconds since last update" % (minute_diff)
+                print "AGENT IS DOWN, %d seconds since last update" % (timediff.seconds)
             else:
                 node.agent_status = True
-                print "AGENT IS UP, %d seconds since last update" % (minute_diff)
+                print "AGENT IS UP, %d seconds since last update" % (timediff.seconds)
         else:
             print "Status has not been updated yet"
     session.commit()
