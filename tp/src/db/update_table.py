f7863e63f7f397b6d36b7a7d9f1e96ddaccf3d79
Allen Sanabria <allen@toppatch.com>
2012-12-05 13:59:03 -0500

    fixed tag_stats, if there arent any nodes in a tag

--- tp/src/db/update_table.py
+++ tp/src/db/update_table.py
@@ -728,17 +728,10 @@ def update_tag_stats(session):
                     session.add(add_tag_stats)
                     session.commit()
             else:
-                tag_stats = session.query(TagStats).\
-                    filter_by(tag_id=tag.id)
-                tag_exists = tag_stats.first()
+                tag_exists = session.query(TagStats).\
+                    filter_by(tag_id=tag.id).first()
                 if tag_exists:
-                    tag_stats.update({"patches_installed" : patchesinstalled,
-                            "patches_available" : patchesuninstalled,
-                            "patches_pending" : patchespending,
-                            "patches_pending" : patchespending,
-                            "reboots_pending" : rebootspending,
-                            "agents_down" : agentsdown,
-                            "agents_up" : agentsup})
+                    session.delete(tag_exists)
                     session.commit()
 
 
