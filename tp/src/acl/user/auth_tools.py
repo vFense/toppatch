eb37f473369b111b32371b30448fd3ab5bb16725
Allen Sanabria <allen@toppatch.com>
2012-12-14 14:04:26 -0500

    Group to User, User to Group Highest WINS

--- tp/src/acl/user/auth_tools.py
+++ tp/src/acl/user/auth_tools.py
@@ -5,6 +5,7 @@ from db.client import validate_session
 from models.user_acl import *
 from models.account import *
 from models.node import *
+from models.tagging import *
 
 from sqlalchemy import or_
 
@@ -19,9 +20,12 @@ class VerifyUser():
         tag_name=None, user_id=None
         ):
         self.session = validate_session(session)
-        self.global_query = self.session.query(GlobalUserAccess)
-        self.node_query = self.session.query(NodeUserAccess)
-        self.tag_query = self.session.query(TagUserAccess)
+        self.global_user_query = self.session.query(GlobalUserAccess)
+        self.global_group_query = self.session.query(GlobalGroupAccess)
+        self.node_user_query = self.session.query(NodeUserAccess)
+        self.node_group_query = self.session.query(NodeGroupAccess)
+        self.tag_user_query = self.session.query(TagUserAccess)
+        self.tag_group_query = self.session.query(TagGroupAccess)
         self.node_id = node_id
         self.host_name = host_name
         self.display_name = display_name
@@ -31,7 +35,7 @@ class VerifyUser():
         self.user_id = user_id
         self.user_name = user_name
         self.action = action
-        self.groups = None
+        self.groups = []
         self.user = None
         self.node = None
         self.tag = None
@@ -56,8 +60,8 @@ class VerifyUser():
                     filter(UsersInAGroup.user_id == self.user.id).\
                     all())
             if len(self.groups) >= 1:
-                self.group_access = self.global_query.\
-                        filter(GlobalUserAccess.group_id.in_(self.groups)).\
+                self.group_access = self.global_group_query.\
+                        filter(GlobalGroupAccess.group_id.in_(self.groups)).\
                         all()
                 if len(self.group_access) >=1:
                     if True in map(lambda gid: gid.is_admin, self.group_access)\
@@ -65,7 +69,7 @@ class VerifyUser():
                         logger.info('User %s is an admin group' % \
                                 (self.user.username))
                         return True
-            self.user_access = self.global_query.\
+            self.user_access = self.global_user_query.\
                     filter(GlobalUserAccess.user_id == self.user.id).\
                     first()
             if self.user_access:
@@ -80,7 +84,7 @@ class VerifyUser():
                     return False
             if self.action in self.operation_call:
                 self._get_acls()
-                permissions = self.has_permission()
+                permissions = self._has_permission()
                 return(permissions)
             else:
                 logger.info('1st else Operation %s is not valid' % (self.action))
@@ -98,7 +102,7 @@ class VerifyUser():
 
     def _set_sql_objects(self):
 
-        if selfhost_name:
+        if self.host_name:
             self.node = self.session.query(NodeInfo).\
                     filter(NodeInfo.host_name == self.host_name).first()
         elif self.display_name:
@@ -129,25 +133,25 @@ class VerifyUser():
         self.user_for_node_exists = None
         self.group_for_node_exists = None
         self.user_for_tag_exists = None
-        self.user_for_nodes_if_tag_doesnt_exists = None
+        self.user_for_nodes_if_tag_does_exists = None
         self.nodes_in_a_tag = None
         self.group_for_tag_exists = None
         self.tag_length_of_nodes = 0
         self.tag_length_of_nodes_that_have_acls = 0
         self.user_for_nodes_if_tag_exists_dict = None
         self.group_for_nodes_if_tag_exists_dict = None
-        if self.node and self.user_access:
-            self.user_for_node_exists = self.node_query.\
+        if self.node:
+            self.user_for_node_exists = self.node_user_query.\
                     filter(NodeUserAccess.node_id == self.node.id).\
                     filter(NodeUserAccess.user_id == self.user.id).first()
             self.user_node_dict = self.user_for_node_exists.__dict__
-        if self.node and self.group_access:
-            self.group_for_node_exists = self.node_query.\
-                    filter(NodeUserAccess.group_id.in_(self.groups)).all()
+        if self.node and len(self.groups) >=1:
+            self.group_for_node_exists = self.node_group_query.\
+                    filter(NodeGroupAccess.group_id.in_(self.groups)).all()
             self.group_node_dict = map(lambda gid: gid.__dict__, 
                     self.group_for_node_exists)
-        if self.tag and self.user_access:
-            self.user_for_tag_exists = self.tag_query.\
+        if self.tag:
+            self.user_for_tag_exists = self.tag_user_query.\
                     filter(TagUserAccess.tag_id == self.tag.id).\
                     filter(TagUserAccess.user_id == self.user.id).first()
             if self.user_for_tag_exists:
@@ -158,7 +162,7 @@ class VerifyUser():
                         filter(TagsPerNode.tag_id == self.tag.id).all())
                 if len(self.nodes_in_a_tag) >=1:
                     self.tag_length_of_nodes = len(self.nodes_in_a_tag)
-                    self.user_for_nodes_if_tag_exists = self.node_query.\
+                    self.user_for_nodes_if_tag_exists = self.node_user_query.\
                             filter(NodeUserAccess.node_id.\
                             in_(self.nodes_in_a_tag)).\
                             filter(NodeUserAccess.user_id == self.user.id).all()
@@ -168,10 +172,10 @@ class VerifyUser():
                         self.user_for_nodes_if_tag_exists_dict = \
                                 map(lambda nodes: nodes.__dict__,\
                                 self.user_for_nodes_if_tag_exists)
-        if self.tag and self.group_access:
-            self.group_for_tag_exists = self.tag_query.\
-                    filter(TagUserAccess.group_id.in_(self.groups)).\
-                    filter(TagUserAccess.tag_id == self.tag.id).all()
+        if self.tag and len(self.groups) >=1:
+            self.group_for_tag_exists = self.tag_group_query.\
+                    filter(TagGroupAccess.group_id.in_(self.groups)).\
+                    filter(TagGroupAccess.tag_id == self.tag.id).all()
             if len(self.group_for_tag_exists) >=1:
                 self.group_tag_dict = map(lambda gid: gid.__dict__, 
                         self.group_for_tag_exists)
@@ -181,113 +185,208 @@ class VerifyUser():
                         filter(TagsPerNode.tag_id == self.tag.id).all())
                 if len(self.nodes_in_a_tag) >=1:
                     self.tag_length_of_nodes = len(self.nodes_in_a_tag)
-                    self.group_for_nodes_if_tag_exists = self.node_query.\
-                            filter(NodeUserAccess.node_id.\
+                    self.group_for_nodes_if_tag_exists = self.node_user_query.\
+                            filter(NodeGroupAccess.node_id.\
                             in_(self.nodes_in_a_tag)).\
-                            filter(NodeUserAccess.group_id.in_(self.groups).all()
+                            filter(NodeGroupAccess.group_id.in_(self.groups)).all()
                     self.tag_length_of_nodes_that_have_acls = \
                             len(self.group_for_nodes_if_tag_exists)
                     if len(self.group_for_nodes_if_tag_exists) >=1:
                         self.group_for_nodes_if_tag_exists_dict = \
                                 map(lambda nodes: nodes.__dict__,\
                                 self.group_for_nodes_if_tag_exists)
+        print self.user_for_node_exists
+        print self.group_for_node_exists
+        print self.user_for_tag_exists
+        print self.user_for_nodes_if_tag_does_exists
+        print self.group_for_nodes_if_tag_does_exists
+        print self.nodes_in_a_tag
+        print self.group_for_tag_exists
+        print self.tag_length_of_nodes
+        print self.tag_length_of_nodes_that_have_acls
+        print self.user_for_nodes_if_tag_exists_dict
+        print self.group_for_nodes_if_tag_exists_dict
 
     def _has_permission(self):
         acl_key = self.action
 
         #Check User Settings
-        if self.user_access:
-            user_hash = self.user_access.__dict__
+        if len(self.groups) == 0:
             #Check Global User Settings
-            if user_hash[acl_key] and user_access.is_global:
-                logger.info('%s - %s has global permission for %s' %\
-                        (self.user.username, self.user.username, acl_key)
-                        )
+            if self.user_access:
+                user_hash = self.user_access.__dict__
+                if user_hash[acl_key] and self.user_access.is_global:
+                    logger.info('%s - %s has global permission for %s' %\
+                            (self.user.username, self.user.username, acl_key)
+                            )
                 #return True
-                self.user_auth.append([self.user.username, 'User', 'Global',
-                    acl_key, True])
+                    self.user_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Global',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': True
+                        })
             #Check Node User Settings
-            if self.user_for_node_exists:
-                if user_node_hash[acl_key] and \
-                        False in user_access.is_admin:
+            elif self.user_for_node_exists:
+                if self.user_node_dict[acl_key]:
                     logger.info('%s - %s has %s permission on node  %s' %\
                             (self.user.username, self.user.username,
                                 acl_key, self.node.ip_address)
                             )
                     #return True
-                    self.user_auth.append([self.user.username, 'User', 'Node',
-                        'Node', acl_key, True])
+                    self.user_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Node',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': True
+                        })
                 else:
                     logger.info('%s - %s doesnt has %s'%\
                             (self.user.username, self.user.username, acl_key)+
                             'permission on node  %s' %\
                             (self.node.ip_address)
                             )
-                    #return False
-                    self.user_auth.append([self.user.username, 'User', 'Node',
-                        'Node', acl_key, False])
+                    self.user_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Node',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': False
+                        })
             #Check Tag User Settings
             if self.user_for_tag_exists:
-                if self.user_tag_dict[acl_key] and \
-                        False in user_access.is_admin:
+                if self.user_tag_dict[acl_key]:
                     logger.info('%s - %s has %s'%\
                             (self.user.username, self.user.username, acl_key)+
                             'permission on this tag  %s' %\
                             (self.tag.tag)
                             )
-                    #return True
-                    self.user_auth.append([self.user.username, 'User', 'Tag',
-                        acl_key, True])
+                    self.user_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Tag',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': True
+                        })
                 else:
                     logger.info('%s - %s doesnt has %s'%\
                             (self.user.username, self.user.username, acl_key)+
                             'permission on this tag  %s' %\
                             (self.tag.tag)
                         )
-                    #return False
-                    self.user_auth.append([self.user.username, 'User', 'Tag',
-                        acl_key, False])
-            if self.user_for_nodes_if_tag_doesnt_exists:
-        #Check Tag Group Settings
-        if self.group_access:
-            group_dict = map(lambda gid: gid.__dict__, self.group_access)
-            #Check Global Group Settings
-            if True in map(lambda gid: gid['is_global'], group_dict) and \
-                    acl_key in map(lambda gid: gid[acl_key], group_dict):
-                logger.info('%s - %s has global permission for %s' %\
-                        (self.user.username, self.user.username, acl_key)
+                    self.user_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Tag',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': False
+                        })
+            if self.user_for_nodes_if_tag_exists:
+                if True in map(lambda uid: uid[acl_key],\
+                            self.user_for_nodes_if_tag_exists_dict:)
+                    logger.info('%s - %s has %s'%\
+                            (self.user.username, self.user.username, acl_key)+
+                            'permission on this tag  %s' %\
+                            (self.tag.tag)
+                            )
+                    self.user_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Tag',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': True
+                        })
+                else:
+                    logger.info('%s - %s doesnt has %s'%\
+                            (self.user.username, self.user.username, acl_key)+
+                            'permission on this tag  %s' %\
+                            (self.tag.tag)
                         )
-                return True
+                    self.user_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Tag',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': False
+                        })
+
+            #if self.user_for_nodes_if_tag_doesnt_exists:
+        #Check Tag Group Settings
+        if len(self.groups) >=1:
+            if self.group_access:
+                group_dict = map(lambda gid: gid.__dict__, self.group_access)
+                #Check Global Group Settings
+                if True in map(lambda gid: gid['is_global'], group_dict) and \
+                        acl_key in map(lambda gid: gid[acl_key], group_dict):
+                    logger.info('%s - %s has global permission for %s' %\
+                            (self.user.username, self.user.username, acl_key)
+                            )
+                    self.user_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Global',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': True
+                        })
             #Check Node Group Settings
             elif self.group_for_node_exists:
-                if True in map(lambda gid: gid[acl_key], group_node_dict):
+                if True in map(lambda gid: gid[acl_key], self.group_node_dict):
                     logger.info('%s - %s has %s permission on node  %s' %\
                             (self.user.username, self.user.username,
                                 acl_key, self.node.ip_address)
                             )
-                    return True
+                    self.group_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Node',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': True
+                        })
                 else:
                     logger.info('%s - %s doesnt has %s'%\
                             (self.user.username, self.user.username, acl_key)+
                             'permission on node  %s' %\
                             (self.node.ip_address)
                             )
-                    return False
+                    self.group_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Node',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': False
+                        })
             #Check Tag Group Settings
             elif self.group_for_tag_exists:
-                if True in map(lambda gid: gid[acl_key], group_tag_dict):
+                if True in map(lambda gid: gid[acl_key], self.group_tag_dict):
                     logger.info('%s - %s has %s'%\
                             (self.user.username, self.user.username, acl_key)+
                             'permission on this tag  %s' %\
                             (self.tag.tag)
                             )
-                    return True
+                    self.group_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Tag',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': True
+                        })
                 else:
                     logger.info('%s - %s doesnt has %s'%\
                             (self.user.username, self.user.username, acl_key)+
                             'permission on this tag  %s' %\
                             (self.tag.tag)
                             )
-                    return False
-        else:
-            return False
+                    self.group_auth.append({
+                        'user': self.user.username, 
+                        'verification_type': 'Tag',
+                        'groups': self.groups,
+                        'acl_action': acl_key,
+                        'action_allowed': False
+                        })
+        print self.user_auth
+        print self.group_auth
+        #else:
+            #return False
+            #return False
