ca0d0131f7e06ca3ddc0acba4014781a9ed55a3b
Allen Sanabria <allen@toppatch.com>
2012-12-16 23:19:00 -0500

    done, peace

--- tp/scripts/initialize_rv.py
+++ tp/scripts/initialize_rv.py
@@ -7,28 +7,28 @@ from user.manager import *
 
 ENGINE = init_engine()
 session = create_session(ENGINE)
-create_group(session, groupname='ADMIN')
-create_group(session, groupname='READ_ONLY')
-create_group(session, groupname='API_RO')
-create_group(session, groupname='API_RW')
+#create_group(session, groupname='ADMIN')
+#create_group(session, groupname='READ_ONLY')
+#create_group(session, groupname='API_RO')
+#create_group(session, groupname='API_RW')
 
 acl_admin = {
-        'group_id': '1', 'isadmin': True, 'isglobal': True,
-        'install': True, 'uninstall': True, 'reboot': True, 'wol': True, 
-        'snapshot_creation': True, 'snapshot_removal':True,
-        'snapshot_revert': True, 'tag_creation': True, 'tag_removal':True
+        'group_id': '1', 'isadmin': 'true', 'isglobal': 'true',
+        'install': 'true', 'uninstall': 'true', 'reboot': 'true', 'wol': 'true', 
+        'snapshot_creation': 'true', 'snapshot_removal':'true',
+        'snapshot_revert': 'true', 'tag_creation': 'true', 'tag_removal':'true'
         }
 
 acl_ro = {
-        'group_id': '3', 'isadmin': False, 'isglobal': True,
-        'install': False, 'uninstall': False, 'reboot': False, 'wol': False, 
-        'snapshot_creation': False, 'snapshot_removal': False,
-        'snapshot_revert': False, 'tag_creation': False, 'tag_removal': False,
-        'readonly': True
+        'group_id': '3', 'isadmin': 'false', 'isglobal': 'true',
+        'install': 'false', 'uninstall': 'false', 'reboot': 'false', 'wol': 'false', 
+        'snapshot_creation': 'false', 'snapshot_removal': 'false',
+        'snapshot_revert': 'false', 'tag_creation': 'false', 'tag_removal': 'false',
+        'readonly': 'true'
         }
-acl_group_a = acl_modifier(session, 'global_group', 'create', **acl_admin)
+acl_group_a = acl_modifier(session, 'global_group', 'create', acl_admin)
 print acl_group_a
-acl_group_b = acl_modifier(session, 'global_group', 'create', **acl_ro)
+acl_group_b = acl_modifier(session, 'global_group', 'create', acl_ro)
 print acl_group_b
 
 a = create_user(session, username='admin', password='toppatch', groupname='ADMIN', user_name='initializer')
