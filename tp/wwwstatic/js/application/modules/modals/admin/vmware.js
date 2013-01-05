f92db56be8e070a976e519fac55502842570621c
Sergio Polo <sergio@toppatch.com>
2013-01-05 17:46:34 -0500

    fixed cycle issue

--- tp/wwwstatic/js/application/modules/modals/admin/vmware.js
+++ tp/wwwstatic/js/application/modules/modals/admin/vmware.js
@@ -20,9 +20,9 @@ define(
             View: Backbone.View.extend({
                 initialize: function () {
                     this.template = myTemplate;
-                    //this.collection = new exports.Collection();
-                    //this.collection.bind('reset', this.render, this);
-                    //this.collection.fetch();
+                    this.collection = new exports.Collection();
+                    this.collection.bind('reset', this.render, this);
+                    this.collection.fetch();
                 },
                 events: {
                     'submit form': 'submit'
@@ -42,7 +42,7 @@ define(
                         $alert.removeClass('alert-success').addClass('alert-error').html('Please fill the required fields.').show();
                         return false;
                     } else {
-                        url += '?' + $hostInput.serialize() + '&' + $cycleInput.serialize() + 'h';
+                        url += '?' + $hostInput.serialize() + '&' + $cycleInput.serialize();
                         url += '&' + $userInput.serialize() + '&' + $passInput.serialize();
                         url += '&' + $ssInput.attr('name') + '=' + $ssInput.prop('checked');
                         that.$el.find('.control-group').removeClass('error');
@@ -64,12 +64,12 @@ define(
                 render: function () {
                     if (this.beforeRender !== $.noop) { this.beforeRender(); }
 
-                    var template = _.template(this.template);
-                        //data = this.collection.toJSON()[0];
-
+                    var template = _.template(this.template),
+                        data = this.collection.toJSON()[0];
+                    window.console.log(data);
                     this.$el.empty();
 
-                    this.$el.html(template());
+                    this.$el.html(template({data: data}));
 
                     if (this.onRender !== $.noop) { this.onRender(); }
                     return this;
