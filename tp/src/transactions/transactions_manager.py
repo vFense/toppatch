3c8884589c8ee3e39c789fbd03cccd29ceea9ea6
Allen Sanabria <allen@toppatch.com>
2013-01-15 10:18:35 -0500

    json return for transactions

--- tp/src/transactions/transactions_manager.py
+++ tp/src/transactions/transactions_manager.py
@@ -20,21 +20,14 @@ def retrieve_transactions(session, count=None, offset=None):
         results = None
         node = None
         node_info = session.query(NodeInfo).filter(NodeInfo.id == trans[1][0].node_id).first()
-        if node_info:
-            if node_info.host_name:
-                node = node_info.host_name
-            elif node_info.display_name:
-                node = node_info.display_name
-            elif node_info.is_vm:
-                vm = session.query(VirtualMachineInfo).\
-                        filter(VirtualMachineInfo.node_id == \
-                        node_info.id).first()
-                if vm:
-                    node = vm.vm_name
-                elif node_info.computer_name:
-                    node = node_info.computer_name
-                elif node_info.ip_address:
-                    node = node_info.ip_address
+        if node_info.is_vm:
+            vm = session.query(VirtualMachineInfo).\
+                filter(VirtualMachineInfo.node_id == \
+                node_info.id).first()
+            if vm:
+                vm_name = vm.vm_name
+        else:
+            vm = None
         if trans[1][0].operation_received:
             operation_received = trans[1][0].operation_received.strftime("%m/%d/%Y %H:%M")
         if len(trans[1]) == 1:
@@ -43,7 +36,11 @@ def retrieve_transactions(session, count=None, offset=None):
                          "username" : trans[1][0].username,
                          "operation_sent" : trans[1][0].operation_sent.strftime("%m/%d/%Y %H:%M"),
                          "operations_received" : operation_received,
-                         "node_id" : node,
+                         "node_id" : node_info.id,
+                         "node_host_name" : node_info.host_name,
+                         "node_vm_name" : vm
+                         "node_ip_address" : node_info.ip_address,
+                         "node_computer_name" : node_info.computer_name,
                          "results_received" : None,
                          "patch_id" : None,
                          "result" : None,
@@ -56,7 +53,11 @@ def retrieve_transactions(session, count=None, offset=None):
                          "username" : trans[1][0].username,
                          "operation_sent" : trans[1][0].operation_sent.strftime("%m/%d/%Y %H:%M"),
                          "operations_received" : operation_received,
-                         "node_id" : node,
+                         "node_id" : node_info.id,
+                         "node_host_name" : node_info.host_name,
+                         "node_vm_name" : vm
+                         "node_ip_address" : node_info.ip_address,
+                         "node_computer_name" : node_info.computer_name,
                          "results_received" : trans[1][1].results_received.strftime("%m/%d/%Y %H:%M"),
                          "patch_id" : trans[1][1].patch_id,
                          "result" : trans[1][1].result,
