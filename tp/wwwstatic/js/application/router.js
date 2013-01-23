a81779614d0ec055794aacf9d72e383889df5f8e
Sergio Polo <sergio@toppatch.com>
2013-01-23 00:51:03 -0500

    created rough version of line chart and modified data values so it works with current api

--- tp/wwwstatic/js/application/router.js
+++ tp/wwwstatic/js/application/router.js
@@ -103,6 +103,7 @@ define(
                 require(['modules/node'], function (myView) {
                     myView.Collection = myView.Collection.extend({id: id});
                     myView.VmCollection = myView.VmCollection.extend({id: id});
+                    myView.GraphCollection = myView.GraphCollection.extend({id: id});
                     var view = new myView.View();
                     that.show({hash: '#nodes', title: 'Nodes', view: view});
                 });
