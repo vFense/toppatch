950e7d46d834afd4e3e0a9a4fe4140785a3f20e8
Sergio Polo <sergio@toppatch.com>
2013-01-25 01:34:01 -0500

    added axis labels under node graph

--- tp/wwwstatic/js/application/d3charts/stackedArea.js
+++ tp/wwwstatic/js/application/d3charts/stackedArea.js
@@ -17,7 +17,7 @@ define(['jquery', 'd3', 'underscore'], function ($, d3, _) {
         function chart(selection) {
             selection.each(function (data) {
                 // generate chart here; `d` is the data and `this` is the element
-                var svg, xAxis, yAxisLeft, line, graph, point, area, stack, key,
+                var svg, xAxis, xAxisText, yAxisLeft, yAxisLeftText, line, graph, point, area, stack, key,
                     txtTop, txtMask, txtRect, txtMiddleTop, txtMiddle, txtMiddleBottom, txtBottom, txtBottomTop,
                     layers = [{name: 'optional'}, {name: 'recommended'}, {name: 'critical'}],
                     color = d3.scale.category20(),
@@ -116,7 +116,7 @@ define(['jquery', 'd3', 'underscore'], function ($, d3, _) {
                     .append("svg:svg")
                     .data([stack[2]])
                     .attr("width", width)
-                    .attr("height", height)// + margin[0] + margin[2])
+                    .attr("height", height + 10)// + margin[0] + margin[2])
                     .style('overflow', 'visible')
                     .append("svg:g")
                     .attr("transform", "translate(" + 35 + "," + 15 + ")");
@@ -127,6 +127,22 @@ define(['jquery', 'd3', 'underscore'], function ($, d3, _) {
                     .attr("stroke", "none")
                     .attr("fill", function (d, i) { return severityColors[i]; })
                     .attr("d", function (d) { return area(d.values); });
+
+                yAxisLeftText = svg.append("text")
+                    .attr("text-anchor", "end")
+                    .attr("dy", ".75em")
+                    .style("font-size", "10px")
+                    .style("font-weight", "bold")
+                    .attr("transform", "translate(" + "-30" + "," + height / 3.5 + ") rotate(-90)")
+                    .text('Packages');
+
+                xAxisText = svg.append("text")
+                    .attr("text-anchor", "end")
+                    .attr("dy", ".75em")
+                    .style("font-size", "10px")
+                    .style("font-weight", "bold")
+                    .attr("transform", "translate(" + width / 2 + "," + 0.88 * height + ")")
+                    .text('Packages installed over time');
 
                 svg.append("svg:g")
                     .attr("class", "x axis")
